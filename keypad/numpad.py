e6431a4e4f4249a33ec8695efbe73a521174914a
roiki <roiki437@gmail.com>
2019-12-20 05:50:54 -0800

    Keypad widget added

--- keypad/numpad.py
+++ keypad/numpad.py
@@ -1,11 +1,68 @@
 # This Python file uses the following encoding: utf-8
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+
+path = os.path.dirname(__file__)
+qtCreatorFile = "dialog.ui"
+
+Ui_Dialog, _ = uic.loadUiType(os.path.join(path,qtCreatorFile))
+a = []
+b=""
+
+class numpad(QtWidgets.QDialog, Ui_Dialog):
 
 
-class numpad(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+       super(numpad, self).__init__()
+       self.setupUi(self)
+
+       self.num0.clicked.connect(lambda: self.button_clicked("0"))
+       self.num1.clicked.connect(lambda: self.button_clicked("1"))
+       self.num2.clicked.connect(lambda: self.button_clicked("2"))
+       self.num3.clicked.connect(lambda: self.button_clicked("3"))
+       self.num4.clicked.connect(lambda: self.button_clicked("4"))
+       self.num5.clicked.connect(lambda: self.button_clicked("5"))
+       self.num6.clicked.connect(lambda: self.button_clicked("6"))
+       self.num7.clicked.connect(lambda: self.button_clicked("7"))
+       self.num8.clicked.connect(lambda: self.button_clicked("8"))
+       self.num9.clicked.connect(lambda: self.button_clicked("9"))
+       self.dot.clicked.connect(lambda: self.button_clicked("."))
+       self.clearButton.clicked.connect(self.clear_text)
+
+       self.buttonBox.accepted.connect(self.accept)
+       self.buttonBox.rejected.connect(self.reject)
+
+
+       self.entryline.setValidator(QtGui.QDoubleValidator(0.001,999.999,3))
+
+    def button_clicked(self, number):
+        global a
+        global b
+        a.append(number)
+        b.join(a)
+        res=b.join(a)
+        self.entryline.setText(res)
+
+    def clear_text(self):
+        global a
+        global b
+        self.entryline.clear()
+        a=[]
+        b=""
+
+
+
+    def results(self):
+        if self.exec_() == QtGui.QDialog.Accepted:
+            result = self.entryline.text()
+            return result
+        else:
+            return None
+
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from PyQt5.QtCore import *
 from PyQt5 import QtWidgets as qt
 from PyQt5.Qsci import QsciScintilla
 
-from .move import move
-from .kinematics import kinematics
-from .communications import communications
-from .calibration import calibration
-from .file_handling import file
+#from move import move
+#from kinematics import kinematics
+#from communications import communications
+#from calibration import calibration
+#from file_handling import file
+#from keypad.numpad import numpad
 
 class Worker(QRunnable):
      def __init__(self, fn, *args, **kwargs):
@@ -31,78 +32,86 @@ class Worker(QRunnable):
             pass
 
 
-class InputNumPad(QDialog,Ui_dialog):
-
-
-    def __init__(self):
-        super(CustomDialog, self).__init__()
-        self.setupUi(self)
-
-    def getResults(self):
-        if self.exec_() == QDialog.Accepted:
-            # get all values
-            val = self.some_widget.some_function()
-            val2 = self.some_widget2.some_another_function()
-            return val1, val2, ...
-        else:
-            return None
 class Ui(qt.QMainWindow):
-
-
-
     def set_parameter(self, param):
         if param == alpha1:
-            global DHa1 = self.dh_alpha1.text()
+            global DHa1
+            DHa1= self.dh_alpha1.text()
         elif param == alpha2:
-            global DHa2 = self.dh_alpha2.text()
+            global DHa2
+            DHa2= self.dh_alpha2.text()
         elif param == alpha3:
-            global DHa3 = self.dh_alpha1.text()
+            global DHa3
+            DHa3= self.dh_alpha3.text()
         elif param == alpha4:
-            global DHa4 = self.dh_alpha1.text()
+            global DHa4
+            DHa4= self.dh_alpha4.text()
         elif param == alpha5:
-            global DHa5 = self.dh_alpha1.text()
+            global DHa5
+            DHa5= self.dh_alpha5.text()
         elif param == alpha6:
-            global DHa6 = self.dh_alpha1.text()
+            global DHa6
+            DHa6= self.dh_alpha6.text()
         elif param == a1:
-            global DHr1 = self.dh_a1.text()
+            global DHr1
+            DHr1= self.dh_a1.text()
         elif param == a2:
-            global DHr2 = self.dh_a2.text()
+            global DHr2
+            DHr2= self.dh_a2.text()
         elif param == a3:
-            global DHr3 = self.dh_a3.text()
+            global DHr3
+            DHr3= self.dh_a3.text()
         elif param == a4:
-            global DHr4 = self.dh_a4.text()
+            global DHr4
+            DHr4 = self.dh_a4.text()
         elif param == a5:
-            global DHr5 = self.dh_a5.text()
+            global DHr5
+            DHr5 = self.dh_a5.text()
         elif param == a6:
-            global DHr6 = self.dh_a6.text()
+            global DHr6
+            DHr6= self.dh_a6.text()
         elif param == d1:
-            global DHd1 = self.dh_d1.text()
+            global DHd1
+            DHd1= self.dh_d1.text()
         elif param == d2:
-            global DHd2 = self.dh_d2.text()
+            global DHd2
+            DHd2= self.dh_d2.text()
         elif param == d3:
-            global DHd3 = self.dh_d3.text()
+            global DHd3
+            DHd3= self.dh_d3.text()
         elif param == d4:
-            global DHd4 = self.dh_d4.text()
+            global DHd4
+            DHd4= self.dh_d4.text()
         elif param == d5:
-            global DHd5 = self.dh_d5.text()
+            global DHd5
+            DHd5= self.dh_d5.text()
         elif param == d6:
-            global DHd6 = self.dh_d6.text()
+            global DHd6
+            DHd6= self.dh_d6.text()
         elif param == theta1:
-            global DHt1 = self.dh_theta1.text()
+            global DHt1
+            DHt1= self.dh_theta1.text()
         elif param == theta2:
-            global DHt2 = self.dh_theta2.text()
+            global DHt2
+            DHt2 =self.dh_theta2.text()
         elif param == theta3:
-            global DHt3 = self.dh_theta3.text()
+            global DHt3
+            DHt3= self.dh_theta3.text()
         elif param == theta4:
-            global DHt4 = self.dh_theta4.text()
+            global DHt4
+            DHt4= self.dh_theta4.text()
         elif param == theta5:
-            global DHt5 = self.dh_theta5.text()
+            global DHt5
+            DHt5= self.dh_theta5.text()
         elif param == theta6:
-            global DHt6 = self.dh_theta6.text()
+            global DHt6
+            DHt6= self.dh_theta6.text()
 
-    def numpadDialog(self):
-        w=InputNumPad()
-        Value = w.getResults()
+   # def numpad(self):
+    #    w=numpad()
+     #   value = w.results()
+      #  if value:
+       #     self.jogmm_box.setValue(value)
 
 
 
@@ -120,33 +129,32 @@ class Ui(qt.QMainWindow):
         self.save_file.clicked.connect(file.saveFile)
         self.new_File.clicked.connect(file.newFile)
 
+        self.getvalue_button.clicked.connect(self.numpad)
+
 
         ##Jogging Buttons##
-        self.jog1pos.clicked.connect(move.jogJoint(J1, 1)
-        self.jog1pos.clicked.connect(move.jogJoint(J2, 1)
-        self.jog1pos.clicked.connect(move.jogJoint(J3, 1)
-        self.jog1pos.clicked.connect(move.jogJoint(J4, 1)
-        self.jog1pos.clicked.connect(move.jogJoint(J5, 1)
-        self.jog1pos.clicked.connect(move.jogJoint(J6, 1)
+        #self.jog1pos.clicked.connect(move.jogJoint(J1, 1)
+        #self.jog2pos.clicked.connect(move.jogJoint(J2, 1)
+        #self.jog3pos.clicked.connect(move.jogJoint(J3, 1)
+        #self.jog4pos.clicked.connect(move.jogJoint(J4, 1)
+        #self.jog5pos.clicked.connect(move.jogJoint(J5, 1)
+        #self.jog6pos.clicked.connect(move.jogJoint(J6, 1)
 
-        self.jog1neg.clicked.connect(move.jogJoint(J1, 0))
-        self.jog2neg.clicked.connect(move.jogJoint(J2, 0))
-        self.jog3neg.clicked.connect(move.jogJoint(J3, 0))
-        self.jog4neg.clicked.connect(move.jogJoint(J4, 0))
-        self.jog5neg.clicked.connect(move.jogJoint(J5, 0))
-        self.jog6neg.clicked.connect(move.jogJoint(J6, 0))
+        #self.jog1neg.clicked.connect(move.jogJoint(J1, 0))
+        #self.jog2neg.clicked.connect(move.jogJoint(J2, 0))
+        #self.jog3neg.clicked.connect(move.jogJoint(J3, 0))
+        #self.jog4neg.clicked.connect(move.jogJoint(J4, 0))
+        #self.jog5neg.clicked.connect(move.jogJoint(J5, 0))
+        #self.jog6neg.clicked.connect(move.jogJoint(J6, 0))
 
-        self.dh_alpha1.textChanged.connect(set_parameter(alpha1))
-        self.dh_alpha1.setValidator(QtGui.QDoubleValidator.StandardNotation())
+        #self.dh_alpha1.textChanged.connect(set_parameter(alpha1))
+        #self.dh_alpha1.setValidator(QtGui.QDoubleValidator.StandardNotation())
 
         self.show()  # Show the GUI
 
 
-
 def main():
-
     app = qt.QApplication(sys.argv)
-
     window = Ui()
     app.exec_()
 
