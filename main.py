2d5ecd149280adb94e88759d56c4deeabc0170a0
roiki <roiki437@gmail.com>
2020-01-08 05:40:06 -0800

    -

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3import sys
+#!/usr/bin/python
 import os, traceback
 import time
 import sys
@@ -9,22 +9,28 @@ from PyQt5 import uic
 from queue import Queue
 import rospy
 
-from move import move
-#from kinematics import kinematics
-#from communications import communications
+from move import Move, Move_Signals
 #from calibration import calibration
 from file_handling import fileHandling
 from keypad.numpad import numpad
+from circle_window import circle_window
+from Rviz.Rviz import Rviz
+from ros_publsher_subsriber import Ros_talkers
 
 program_queue = Queue()
 robot_paused = False
 robot_stopped = False
+jog_increment = 1
+
 class WorkerSignals(QObject):
     finished = pyqtSignal()
     error = pyqtSignal(tuple)
     result = pyqtSignal(object)
     progress = pyqtSignal(int)
-    running = pyqtSignal(bool)
+    running = pyqtSignal()
+    is_paused = pyqtSignal()
+    
+    
 class Worker(QRunnable):
      def __init__(self, fn, *args, **kwargs):
              super(Worker, self).__init__()
@@ -39,11 +45,7 @@ class Worker(QRunnable):
      @pyqtSlot
      def run(self):
         try:
-            result = self.fn(
-                *self.args, **self.kwargs,
-                status=self.signals.status,
-                progress=self.signals.progress
-            )
+            result = self.fn(*self.args, **self.kwargs)
         except:
             traceback.print_exc()
             exctype, value = sys.exc_info()[:2]
@@ -61,43 +63,65 @@ class Ui(QMainWindow):
         uic.loadUi(ui_file, self)  # Load the .ui file
         self.threadpool = QThreadPool()
 
+        self.numpad = numpad()
+
         ## File handling buttons ##
         self.load_file.clicked.connect(fileHandling.loadFile)
         self.save_file.clicked.connect(fileHandling.saveFile)
         self.new_File.clicked.connect(fileHandling.newFile)
 
-        self.getvalue_button.clicked.connect(self.launchDialog)
 
+        #Key dialog for jog distance
+        self.getvalue_button.clicked.connect(self.launchKeypadDialog)
+        numpad.result.connect(self.keypad_results)
+
+        #loop program signal.
+        Move_Signals.connect(self.start_program)
+
+        self.circTeachButton.clicked.connect(self.launchCircleDialog)
+
+        Ros_talkers.encoder_feedback.connect(self.update_encoder_displays)
 
         ##Jogging Buttons##
-        #self.jog1pos.clicked.connect(lambda: move.jogJoint(J1, 1)
-        #self.jog2pos.clicked.connect(lambda:move.jogJoint(J2, 1)
-        #self.jog3pos.clicked.connect(lambda:move.jogJoint(J3, 1)
-        #self.jog4pos.clicked.connect(lambda:move.jogJoint(J4, 1)
-        #self.jog5pos.clicked.connect(lambda:move.jogJoint(J5, 1)
-        #self.jog6pos.clicked.connect(lambda:move.jogJoint(J6, 1)
+        self.jog1pos.clicked.connect(lambda:move.jogJoint(1, 1))
+        self.jog2pos.clicked.connect(lambda:move.jogJoint(2, 1))
+        self.jog3pos.clicked.connect(lambda:move.jogJoint(3, 1))
+        self.jog4pos.clicked.connect(lambda:move.jogJoint(4, 1))
+        self.jog5pos.clicked.connect(lambda:move.jogJoint(5, 1))
+        self.jog6pos.clicked.connect(lambda:move.jogJoint(6, 1))
 
-        #self.jog1neg.clicked.connect(lambda:move.jogJoint(J1, 0))
-        #self.jog2neg.clicked.connect(lambda:move.jogJoint(J2, 0))
-        #self.jog3neg.clicked.connect(lambda:move.jogJoint(J3, 0))
-        #self.jog4neg.clicked.connect(lambda:move.jogJoint(J4, 0))
-        #self.jog5neg.clicked.connect(lambda:move.jogJoint(J5, 0))
-        #self.jog6neg.clicked.connect(lambda:move.jogJoint(J6, 0))
+        self.jog1neg.clicked.connect(lambda:move.jogJoint(1, 0))
+        self.jog2neg.clicked.connect(lambda:move.jogJoint(2, 0))
+        self.jog3neg.clicked.connect(lambda:move.jogJoint(3, 0))
+        self.jog4neg.clicked.connect(lambda:move.jogJoint(4, 0))
+        self.jog5neg.clicked.connect(lambda:move.jogJoint(5, 0))
+        self.jog6neg.clicked.connect(lambda:move.jogJoint(6, 0))
 
         self.jog_increment001.clicked.connect(lambda: self.jogIncrementSelection(0.01))
         self.jog_increment01.clicked.connect(lambda: self.jogIncrementSelection(0.01))
         self.jog_increment1.clicked.connect(lambda: self.jogIncrementSelection(1))
-        elf.jog_increment10.clicked.connect(lambda: self.jogIncrementSelection(10))
+        self.jog_increment10.clicked.connect(lambda: self.jogIncrementSelection(10))
 
-        self.show()  # Show the GUI
-    def launchDialog(self):
-        dlg = numpad()
-        if dlg.exec():
-            value = numpad.result(self)
-            self.jogDistanceBox.setValue(float(value))
+    @pyqtSlot
+    def update_encoder_displays(self, data):
+        self.j1pos.update(data[0])
+        self.j2pos.update(data[1])
+        self.j3pos.update(data[2])
+        self.j4pos.update(data[3])
+        self.j5pos.update(data[4])
+        self.j6pos.update(data[5])
+
+    def launchKeypadDialog(self):
+        self.numpad.show()
+        
+    def launchCircleDialog(self):
+        self.circle_window.show()
 
     def jogIncrementSelection(self, number):
+        global jog_increment
+
         jog_increment = number
+
         if number == 0.01:
             self.jog_increment001.setStyleSheet("backround-color: blue")
             self.jog_increment01.setStyleSheet("background-color: rgb(186, 189, 182)")
@@ -138,18 +162,28 @@ class Ui(QMainWindow):
         if robot_paused is False:
             robot.pause_movement
             robot_paused = True
+            WorkerSignals.is_paused.emit()
             rospy.loginfo("Robot Movement Paused")
         else:
             robot.resume_movement
             robot_paused = False
+            WorkerSignals.running.emit()
             rospy.loginfo("Robot movement Resumed")
 
     def stop_movement(self):
         robot.stop_movement
         rospy.loginfo("Robot movement Stopped")
+        WorkerSignals.finished.emit()
+    
 
-    def teach_position(self):
-        position = robot.get.current_pose()
+    def clear_program(self):
+        global open_program
+        open_program = []
+        self.programView.clear()
+
+    @pyqtSlot(float)
+    def keypad_results(self, number):
+        self.jogDistanceBox.setValue(number)
     
 
 
@@ -158,8 +192,11 @@ class Ui(QMainWindow):
 
 app = QApplication(sys.argv)
 window = Ui()
-worker=Worker(self.runProgramThread)
-
-self.threadpool.start(worker)
+worker1 = Worker(self.runProgramThread)
+worker2 = Worker(Ros_talkers.listener)
+self.threadpool.start(worker1)
+self.threadpool.start(worker2)
 rospy.init_node("robot_program_node")
 app.exec_()
+
+
